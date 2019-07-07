d6c3b9b0f736f3ebb1df0db91d4f2a27f76ad3ea
Justin Cook <justin.cook.dev@gmail.com>
2019-07-07 13:56:16 -0700

    Students reducer.

--- client/reducers/students.js
+++ client/reducers/students.js
@@ -1,5 +1,26 @@
+import {
+  GOT_STUDENTS,
+  CREATED_STUDENT,
+  UPDATED_STUDENT,
+  DELETED_STUDENT,
+} from '../actions/students';
+
 export default (state = [], action) => {
   switch (action.type) {
+    case GOT_STUDENTS: {
+      return action.students;
+    }
+    case CREATED_STUDENT: {
+      return [...state, action.student];
+    }
+    case UPDATED_STUDENT: {
+      return state.map(student =>
+        student.id === action.student.id ? action.student : student
+      );
+    }
+    case DELETED_STUDENT: {
+      return state.filter(({ id }) => id !== action.id);
+    }
     default:
       return state;
   }
